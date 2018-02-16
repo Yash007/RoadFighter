5914980fb463902462e5dc69ecf162c7cc92b05e
YASH SOMPURA <sompurayash@live.com>
2018-02-16 12:57:18 -0500

    Police car updated and also added sound

--- Road Fighter/Objects/Police.swift
+++ Road Fighter/Objects/Police.swift
@@ -14,7 +14,7 @@ class Police: GameObject {
     var soundLock: Bool = false
     init()  {
         //initialize the object with an Image
-        super.init(imageString: "police", initialScale: 1.0)
+        super.init(imageString: "Police", initialScale: 1.0)
         Start()
     }
     
@@ -30,21 +30,24 @@ class Police: GameObject {
     
     override func Reset()    {
         self.position.y = 1000 + self.height!
-        let startFrom: CGFloat = CGFloat(self.halfWidth! + 20.0)
+        let startFrom: CGFloat = CGFloat((screenWidth! * 0.5) + 10)
         let toFrom: CGFloat = CGFloat(screenWidth! - self.halfWidth! - 20.0)
-        let randomX: UInt32 = arc4random_uniform(UInt32(toFrom)) + UInt32(startFrom)
+        let randomX: UInt32 = arc4random_uniform(UInt32(toFrom - startFrom)) + UInt32(startFrom)
         self.position.x = CGFloat(randomX)
         soundLock = false
+        print("\(startFrom) --- \(toFrom)");
+        print(randomX)
+        
     }
     
     override func CheckBounds() {
-        if (self.position.y < (0 + self.height!))    {
+        if (self.position.y < (0 - self.halfHeight!))    {
             self.Reset()
         }
         if(self.position.y > 0 && self.position.y < (667 + self.height!))   {
             if(soundLock == false)  {
                 soundLock = true
-                scene?.run(SKAction.playSoundFileNamed("ambulance_siren", waitForCompletion: false))
+                scene?.run(SKAction.playSoundFileNamed("police_siren", waitForCompletion: false))
             }
         }
     }
