36aeeeba3c6ece317b1ce1e059ab880e5ffb33c8
Yash007 <sompurayash@live.com>
2018-02-23 12:52:10 -0500

    GameTutorial Scene Added

---
+++ Road Fighter/Scenes/GameTutorial.swift
@@ -0,0 +1,72 @@
+//
+//  GameScene.swift
+//  Road Fighter
+//
+//  Created by YASH SOMPURA on 2018-02-07.
+//  Copyright © 2018 YASH SOMPURA. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+import UIKit
+import AVFoundation
+import CoreMotion
+
+class GameTutorial: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
+        //        ScoreManager.Lives = 5
+        //        ScoreManager.Score = 0
+        
+        
+        if let view = self.view {
+            if let scene = SKScene(fileNamed: "GameStartScene") {
+                scene.scaleMode = .aspectFit
+                view.presentScene(scene)
+            }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        //self.roadSprite?.Update()
+        
+    }
+}
+
+
